acf29554520b4304ab8c01783e2b4dfed336abf1
matthewsoto95 <matthewsoto95@gmail.com>
2017-04-18 14:04:00 -0400

    add new file

---
+++ Butget calculator/Js.js
@@ -0,0 +1,7 @@
+function runit() {
+	var F = document.getElementById("a").value;
+	var G = document.getElementById("b").value;
+	var H = document.getElementById("c").value;
+	var I = document.getElementById("d").value;
+	alert(F-G-H-I)
+};
